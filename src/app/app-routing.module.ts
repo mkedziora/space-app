338287f770f2b79bed5dc4009612bbfc0a4cfbe4
Marcin Kedziora <mk.marcin12@gmail.com>
2020-02-24 22:53:22 +0000

    update to Angular 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { BlackHoleComponent } from './black-hole/black-hole.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'space', pathMatch: 'full'},
-  {path: 'intel', loadChildren: './intel/intel.module#IntelModule'},
+  {path: 'intel', loadChildren: () => import('./intel/intel.module').then(m => m.IntelModule)},
   {path: '**', component: BlackHoleComponent},
 ];
 
